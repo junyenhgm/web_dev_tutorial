02fea8d055c38956dc3888bee29da6b0a9da820a
Fumitosi-V3 <cucumber6717@gmail.com>
2020-07-12 19:51:05 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+  var btn = document.getElementById("btn");
+  btn.addEventListener("click", function() {
+    // document.body.style.background = "green";
+    document.getElementById("demo").innerHTML = "Button trigger";
+  });
+
+
+  // document.getElementById("btn").addEventListener("click", displayDate);
+  //
+  // function displayDate() {
+  //   document.getElementById("demo").innerHTML = "Button trigger";
+  //   document.body.style.background = "green";
+  // }
+
+}
